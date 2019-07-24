c768123e2f1fe1b5f9ab072a4ec9b78c70d3aded
Aliaksandr.Kavalevich2 <Aliaksandr.Kavalevich2@thomsonreuters.com>
2019-07-24 18:07:01 +0300

    reformated code

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,103 +12,78 @@ import org.elasticsearch.client.RestHighLevelClient;
 import java.io.IOException;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-
 public class App {
 
-    private static final String HOST = "alpha-analyticssearch-search-elastic.int.thomsonreuters.com";
-    private static final int PORT_ONE = 9200;
-    private static final int PORT_TWO = 9201;
-    private static final String SCHEME = "http";
+  private static final String HOST = "alpha-analyticssearch-search-elastic.int.thomsonreuters.com";
+  private static final int PORT_ONE = 9200;
+  private static final int PORT_TWO = 9201;
+  private static final String SCHEME = "http";
 
-    private static RestHighLevelClient restHighLevelClient;
-    private static final ObjectMapper objectMapper =
-            new ObjectMapper()
-                    .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+  private static RestHighLevelClient restHighLevelClient;
+  private static final ObjectMapper objectMapper =
+      new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
-    private static final String INDEX = "cat_pc_orgs_0.0.3";
-    private static final String TYPE = "orgs";
-    private static final RequestOptions COMMON_OPTIONS;
+  private static final String INDEX = "cat_pc_orgs_0.0.3";
+  private static final String TYPE = "orgs";
+  private static final RequestOptions COMMON_OPTIONS;
 
-    static {
-        RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder();
-        builder.
-                setHttpAsyncResponseConsumerFactory(
-                        new HttpAsyncResponseConsumerFactory
-                                .HeapBufferedResponseConsumerFactory(30 * 1024 * 1024)
+  static {
+    RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder();
+    builder.setHttpAsyncResponseConsumerFactory(
+        new HttpAsyncResponseConsumerFactory.HeapBufferedResponseConsumerFactory(30 * 1024 * 1024));
 
-                );
-        COMMON_OPTIONS = builder.build();
+    COMMON_OPTIONS = builder.build();
+  }
+
+  public static void main(String[] args) {
+    getResult();
+  }
+
+  private static void getResult() {
+    System.out.println("Getting document...");
+    makeConnection();
+    String id = "5000734792";
+    Orgs docFromES = getORGSById(id);
+    System.out.println("Person from DB  --> " + docFromES);
+
+    try {
+      closeConnection();
+    } catch (IOException e) {
+      e.getLocalizedMessage();
+    }
+  }
+
+  private static Orgs getORGSById(String id) {
+
+    GetRequest getPersonRequest = new GetRequest(INDEX, TYPE, id);
+    GetResponse getResponse = null;
+
+    try {
+      getResponse = restHighLevelClient.get(getPersonRequest, COMMON_OPTIONS);
+    } catch (java.io.IOException e) {
+      e.getLocalizedMessage();
     }
 
-    public static void main(String[] args) {
-        getResult();
+    Orgs orgs = new Orgs();
+    if (getResponse != null) {
+      orgs = objectMapper.convertValue(getResponse.getSourceAsMap(), Orgs.class);
     }
 
-    private static void getResult() {
-        System.out.println("Getting document...");
-        makeConnection();
-        String id = "5000734792";
-        Orgs docFromES = getORGSById(id);
-        System.out.println("Person from DB  --> " + docFromES);
+    return orgs.setDocID(id);
+  }
 
+  private static void makeConnection() {
 
-        try {
-            closeConnection();
-        } catch (IOException e) {
-            e.getLocalizedMessage();
-        }
+    if (restHighLevelClient == null) {
+      restHighLevelClient =
+          new RestHighLevelClient(
+              RestClient.builder(
+                  new HttpHost(HOST, PORT_ONE, SCHEME), new HttpHost(HOST, PORT_TWO, SCHEME)));
     }
+  }
 
-    private static Orgs getORGSById(String id) {
-
-        GetRequest getPersonRequest = new GetRequest(
-                INDEX,
-                TYPE,
-                id);
-        GetResponse getResponse = null;
-
-        try {
-            getResponse = restHighLevelClient.get(getPersonRequest, COMMON_OPTIONS);
-        } catch (java.io.IOException e) {
-            e.getLocalizedMessage();
-        }
-
-        Orgs orgs = new Orgs();
-        if (getResponse != null) {
-            orgs = objectMapper.convertValue(getResponse.getSourceAsMap(), Orgs.class);
-        }
-
-
-        return orgs.setDocID(id);
-
-        //        System.out.println("Fields = " + getResponse.getFields());
-//        System.out.println("id = " + getResponse.getId());
-//        System.out.println("Source = " +getResponse.getSource());
-
-//        return
-//                new Orgs()
-//                .setCommonName(getResponse.getSource().get("CommonName").toString())
-//                .setCommonName(getResponse.getSource().get("DocumentTitle").toString())
-//                .setDocID(getResponse.getId());
-
-    }
-
-
-
-    private static void makeConnection() {
-
-        if (restHighLevelClient == null) {
-            restHighLevelClient = new RestHighLevelClient(
-                    RestClient.builder(
-                            new HttpHost(HOST, PORT_ONE, SCHEME),
-                            new HttpHost(HOST, PORT_TWO, SCHEME)));
-        }
-    }
-
-    private static synchronized void closeConnection() throws IOException {
-        restHighLevelClient.close();
-        restHighLevelClient = null;
-    }
-
-
+  private static synchronized void closeConnection() throws IOException {
+    restHighLevelClient.close();
+    restHighLevelClient = null;
+  }
 }
